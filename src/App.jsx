ac24a28e9b09669bd15197ffcbebe7c07bfbf55e
Anmol Bansal <anmol13200159@gmail.com>
2024-11-23 15:40:03 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar.jsx';
 import Homepage from './components/Homepage/Homepage.jsx';
 import OffTrip from './components/OffTrip/OffTrip.jsx';
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
-  return (<>
+  return (
+    <>
       <Navbar/>
-      <Homepage/>
-      <OffTrip/>
-    </>)
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/offtrip" element={<OffTrip />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
--- src/components/OffTrip/OffTrip.jsx
+++ src/components/OffTrip/OffTrip.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import Navbar from '../Navbar/Navbar'
+
+
 export default function OffTrip() {
   return (
     <div>
-        <h1>OffTrip</h1>
+        Plan trip
+
+
     </div>
   )
 }
