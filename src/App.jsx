636a7b1aa63f2aa9e08d02e5261c02dbfc965b38
reaper-js <jatinsaini191@gmail.com>
2024-11-24 17:21:34 +0530

    added OnTrip

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar.jsx';
 import Homepage from './components/Homepage/Homepage.jsx';
 import OffTrip from './components/OffTrip/OffTrip.jsx';
+import OnTrip from './components/OnTrip/OnTrip';
 function App() {
   return (<>
       <Navbar/>
-      <Homepage/>
-      <OffTrip/>
+      {/* <Homepage/> */}
+      {/* <OffTrip/> */}
+      <OnTrip />
     </>)
 }
 
